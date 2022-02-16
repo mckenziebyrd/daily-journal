3c071ade0a007e14f565b4a07b9a8d57eda341a4
mckenziebyrdgranda <mckenziebyrd@me.com>
2022-02-15 20:13:47 -0600

    added js files

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
 import { getJournalEntries } from "./database.js";
+import { DailyJournal } from "./DailyJournal.js"
+
+const container = document.querySelector("#entries")
+
+const render = () => {
+    container.innerHTML = DailyJournal()
+}
